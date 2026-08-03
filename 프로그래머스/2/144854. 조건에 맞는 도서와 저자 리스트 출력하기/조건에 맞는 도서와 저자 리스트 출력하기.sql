dba9e89289b5d62419d4955d8dfb42cf37684a3d
test-ssafy <dnlsemthd27@gmail.com>
2026-08-03 15:40:51 +0900

    [level 2] Title: 조건에 맞는 도서와 저자 리스트 출력하기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/2/144854. 조건에 맞는 도서와 저자 리스트 출력하기/조건에 맞는 도서와 저자 리스트 출력하기.sql
@@ -0,0 +1,6 @@
+select B.BOOK_ID, A.AUTHOR_NAME, date_format(B.PUBLISHED_DATE, '%Y-%m-%d') as PUBLISHED_DATE
+from BOOK B
+    join AUTHOR A
+    on B.AUTHOR_ID = A.AUTHOR_ID
+where B.CATEGORY = '경제'
+order by PUBLISHED_DATE
