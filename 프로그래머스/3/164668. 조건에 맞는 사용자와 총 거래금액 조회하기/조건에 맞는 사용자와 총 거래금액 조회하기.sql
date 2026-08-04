c35d854e752508acc5f947e5a2dfde4156b1c4fd
test-ssafy <dnlsemthd27@gmail.com>
2026-08-04 16:42:29 +0900

    [level 3] Title: 조건에 맞는 사용자와 총 거래금액 조회하기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/3/164668. 조건에 맞는 사용자와 총 거래금액 조회하기/조건에 맞는 사용자와 총 거래금액 조회하기.sql
@@ -0,0 +1,8 @@
+select U.USER_ID, U.NICKNAME, sum(B.PRICE) as TOTAL_SALES
+from USED_GOODS_BOARD B
+    join USED_GOODS_USER U
+    on B.WRITER_ID = U.USER_ID
+where B.STATUS = 'DONE'
+group by U.USER_ID, U.NICKNAME
+having sum(B.PRICE) >= 700000
+order by TOTAL_SALES
