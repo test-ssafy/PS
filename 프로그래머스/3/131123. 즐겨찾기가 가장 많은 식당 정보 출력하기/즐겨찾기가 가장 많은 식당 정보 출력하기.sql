17b1e6b6e34f97a82c8b9a32e831c521ce265d49
test-ssafy <dnlsemthd27@gmail.com>
2026-08-04 13:27:26 +0900

    [level 3] Title: 즐겨찾기가 가장 많은 식당 정보 출력하기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/3/131123. 즐겨찾기가 가장 많은 식당 정보 출력하기/즐겨찾기가 가장 많은 식당 정보 출력하기.sql
@@ -0,0 +1,11 @@
+select FOOD_TYPE, REST_ID, REST_NAME, FAVORITES
+from REST_INFO
+where FAVORITES in (
+    select max(FAVORITES)
+    from REST_INFO
+    group by FOOD_TYPE
+)
+group by FOOD_TYPE
+order by FOOD_TYPE desc
+
+#윈도우 함수로도 풀어보기
