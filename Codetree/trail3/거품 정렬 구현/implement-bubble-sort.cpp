7052734a6e5e8d93406a8c175381a37fbd0f5ac8
test-ssafy <dnlsemthd27@gmail.com>
2026-09-23 15:39:31 +0900

    [Passed] 거품 정렬 구현 (2ms, 0MB)

---
+++ Codetree/trail3/거품 정렬 구현/implement-bubble-sort.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    vector<int> v(n,0);
+    for (int i=0; i<n; i++) cin >> v[i];
+    sort(v.begin(), v.end());
+
+    for (int i : v) cout << i << " ";
+
+    return 0;
+}
